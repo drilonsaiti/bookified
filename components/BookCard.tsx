41a55f3d50f02f5a2b8a5b38d50120c625239363
drilonsaiti <drilon-saiti@hotmail.com>
2026-04-24 17:26:50 +0200

    fixes

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -2,26 +2,28 @@ import Link from "next/link";
 import Image from "next/image";
 import {BookCardProps} from "@/types";
 
-const BookCard = ({ title, author, coverURL, slug }: BookCardProps) => {
+const BookCard = ({title, author, coverURL, slug}: BookCardProps) => {
     return (
         <Link href={`/books/${slug}`}>
             <article className="book-card">
 
-                    <figure className="book-card-figure">
-                        <div className="book-card-cover-wrapper">
-                            <Image src={coverURL} alt={title} width={133} height={200} className="book-card-cover" />
-                        </div>
-                    </figure>
+                <figure className="book-card-figure">
+                    <div className="book-card-cover-wrapper">
+                        <Image src={coverURL} alt={title} width={133} height={200} className="book-card-cover"/>
+                    </div>
 
-                <figcaption className="book-card-meta">
-                    <h3 className="book-card-title">
-                        {title}
-                    </h3>
-                    <p className="book-card-author">
-                        {title}
-                    </p>
+                    <figcaption className="book-card-meta">
+                        <h3 className="book-card-title">
+                            {title}
+                        </h3>
+                        <p className="book-card-author">
+                            {title}
+                        </p>
 
-                </figcaption>
+                    </figcaption>
+                </figure>
+
+
             </article>
         </Link>
     )
